4020b644d3dedd8bffac94cb8b12ffc8c7730a32
Luis Cedillo Maldonado <luis.cedillo.maldonado@proton.me>
2025-02-09 11:57:20 -0600

    installed router captabilities

--- alba/src/main.ts
+++ alba/src/main.ts
@@ -1,5 +1,7 @@
 import { Envelope } from "./components/Envelope/Envelope";
+import { Heart } from "./components/Heart/Heart"
 
 document.addEventListener("DOMContentLoaded", () => {
   Envelope();
+  Heart();
 });
