e0084d566c3e54f98cd0ef4a4c95aaaf33c31544
Luis Cedillo Maldonado <luis.cedillo.maldonado@proton.me>
2025-02-09 16:09:53 -0600

    add allowed servers

--- alba/vite.config.ts
+++ alba/vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5175,
+    allowedHosts: ['code.luishomeserver.com', "alba.luishomeserver.com"], // Add the allowed host here
   },
   build: {
     outDir: 'dist',
